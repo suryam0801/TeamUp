be2ecfe77fe4de7b1d59baa6c43b7ddb970687dd
Padhmanathan R <rpadhmanathan08@gmail.com>
2020-03-29 18:16:13 +0530

    finishes login

--- app/src/main/java/com/example/teamup/MainActivity.java
+++ app/src/main/java/com/example/teamup/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.teamup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentUser=FirebaseAuth.getInstance();
+
+
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(currentUser.getCurrentUser()==null)
+        {
+            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            finish();
+        }
+
+
     }
 }
