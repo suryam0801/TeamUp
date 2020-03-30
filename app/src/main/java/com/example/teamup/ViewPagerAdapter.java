e222be1cb4d9921a6315a59e3cada831a4e4a2be
Gowtham K K <kkgowtham1234@gmail.com>
2020-03-30 23:36:31 +0530

    WIP Workbench

---
+++ app/src/main/java/com/example/teamup/ViewPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.teamup;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm,FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Bundle b=new Bundle();
+        b.putInt("pos", position);
+        WorkBenchFragment fragment = new WorkBenchFragment();
+        fragment.setArguments(b);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position == 0){
+            return "My Projects";
+        }
+        return "Working Projects";
+    }
+}
