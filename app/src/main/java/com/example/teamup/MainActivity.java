d1195aecd136459b43b97a7e46e5506cc66f4483
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-04-03 22:58:01 +0530

    Fixed Explore View

--- app/src/main/java/com/example/teamup/MainActivity.java
+++ app/src/main/java/com/example/teamup/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.teamup;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,15 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         currentUser=FirebaseAuth.getInstance();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //changed for testing purpose
-                //currentUser.signOut();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                finish();
-            }
-        });
+        if(currentUser.getCurrentUser()!=null)
+        {
+            startActivity(new Intent(MainActivity.this, ExploreActivity.class));
+            finish();
+        } else {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
     }
 
 
@@ -39,11 +37,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        if(currentUser.getCurrentUser()==null)
+        if(currentUser.getCurrentUser()!=null)
         {
+            startActivity(new Intent(MainActivity.this, ExploreActivity.class));
+            finish();
+        } else {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
         }
-
     }
 }
