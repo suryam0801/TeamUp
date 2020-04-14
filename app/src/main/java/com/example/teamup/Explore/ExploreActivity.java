38a1d76f007fc8e2f249f568f9fc7d00a98f63d2
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-04-14 17:24:20 +0530

    ExploreActivity List View Ui Updated

--- app/src/main/java/com/example/teamup/Explore/ExploreActivity.java
+++ app/src/main/java/com/example/teamup/Explore/ExploreActivity.java
@@ -1,24 +1,24 @@
 package com.example.teamup.Explore;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.baoyz.swipemenulistview.SwipeMenu;
-import com.baoyz.swipemenulistview.SwipeMenuCreator;
-import com.baoyz.swipemenulistview.SwipeMenuItem;
-import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.example.teamup.ControlPanel.ControlPanel;
 import com.example.teamup.ControlPanel.DisplayApplicants.Applicant;
 import com.example.teamup.CreateProject;
@@ -42,7 +42,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public class ExploreActivity extends AppCompatActivity implements Dialogue.DialogueInterfaceListener {
+public class ExploreActivity extends Activity {
 
     public final String TAG=ExploreActivity.this.getClass().getSimpleName();
     FirebaseFirestore db=FirebaseFirestore.getInstance();
@@ -51,9 +51,9 @@ public class ExploreActivity extends AppCompatActivity implements Dialogue.Dialo
     private List<Project> ProjectList;
     ProgressBar progressBar;
     Project projects;
-    int globalIndex;
     private ProjectAdapter adapter;
     Button createProject,workbench;
+    Dialog dialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,6 +63,7 @@ public class ExploreActivity extends AppCompatActivity implements Dialogue.Dialo
         currentUser=FirebaseAuth.getInstance();
         createProject = findViewById(R.id.addproject);
         workbench=findViewById(R.id.workbench);
+        dialog = new Dialog(ExploreActivity.this);
 
 
         createProject.setOnClickListener(new View.OnClickListener() {
@@ -86,8 +87,7 @@ public class ExploreActivity extends AppCompatActivity implements Dialogue.Dialo
 
     public void loadprojectlist()
     {
-        lvproject=findViewById(R.id.listview);
-
+        lvproject=findViewById(R.id.listview_explore_projects);
         ProjectList=new ArrayList<>();
         if (ProjectList.size()>0)
             ProjectList.clear();
@@ -110,6 +110,14 @@ public class ExploreActivity extends AppCompatActivity implements Dialogue.Dialo
 
                             progressBar.setVisibility(View.INVISIBLE);
 
+                            lvproject.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                    Toast.makeText(ExploreActivity.this,i + "",Toast.LENGTH_LONG).show();
+                                    showDialogue(i);
+                                }
+                            });
+
                         }else {
                             Log.d("Logger","No such Valid Item");
                             Toast.makeText(ExploreActivity.this,"No such Document",Toast.LENGTH_LONG).show();
@@ -125,14 +133,37 @@ public class ExploreActivity extends AppCompatActivity implements Dialogue.Dialo
         });
     }
 
-    private void apply(String projectId) {
-        Dialogue dialog=new Dialogue(projectId);
-        dialog.show(getSupportFragmentManager(),"Dialogue");
-    }
+    public void showDialogue(final int pos){
+        dialog.setContentView(R.layout.explore_project_display_popup);
 
-    @Override
-    public void applydesc(String shortdesc,String projectId) {
-            saveApplicant(shortdesc,projectId);
+        TextView projectName = dialog.findViewById(R.id.popup_project_name);
+        TextView creatorName = dialog.findViewById(R.id.popup_project_creator);
+        TextView projectShortDescription = dialog.findViewById(R.id.popup_project_short_description);
+        TextView projectLongDescription = dialog.findViewById(R.id.popup_project_long_description);
+        Button cancelButton = dialog.findViewById(R.id.popup_cancel_button);
+        Button acceptButton = dialog.findViewById(R.id.popup_accept_button);
+
+        projectName.setText(ProjectList.get(pos).getProjectName());
+        creatorName.setText("Created By: " + ProjectList.get(pos).getCreatorName());
+        projectShortDescription.setText(ProjectList.get(pos).getProjectDescription());
+        projectLongDescription.setText("Looks like this project's creator has not provided a detailed description.");
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //call apply dialogue
+            }
+        });
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        dialog.show();
     }
 
     @Override
