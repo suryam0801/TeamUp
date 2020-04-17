a7eb33897a6a1f5c971f3ea752a4c707da4e491e
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-04-17 21:07:15 +0530

    ExploreActivity updated UI

--- app/src/main/java/com/example/teamup/MainActivity.java
+++ app/src/main/java/com/example/teamup/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.teamup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.teamup.login.LoginActivity;
