f36f5fc80438780eae5de376f464ec9341421149
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-04-26 16:09:08 +0530

    Notifications Finished

--- app/src/main/java/com/example/teamup/Notification/NotificationActivity.java
+++ app/src/main/java/com/example/teamup/Notification/NotificationActivity.java
@@ -115,8 +115,8 @@ public class NotificationActivity extends AppCompatActivity {
                             previousListView.setAdapter(adapterPrevious);
                             thisWeekListView.setAdapter(adapterThisWeek);
 
-                            ListUtils.setDynamicHeight(previousListView);
-                            ListUtils.setDynamicHeight(thisWeekListView);
+                            Utility.setListViewHeightBasedOnChildren(thisWeekListView);
+                            Utility.setListViewHeightBasedOnChildren(previousListView);
 
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
@@ -125,24 +125,27 @@ public class NotificationActivity extends AppCompatActivity {
                 });
     }
 
-    public static class ListUtils {
-        public static void setDynamicHeight(ListView mListView) {
-            ListAdapter mListAdapter = mListView.getAdapter();
-            if (mListAdapter == null) {
-                // when adapter is null
+    public static class Utility {
+
+        public static void setListViewHeightBasedOnChildren(ListView listView) {
+            ListAdapter listAdapter = listView.getAdapter();
+            if (listAdapter == null) {
+                // pre-condition
                 return;
             }
-            int height = 0;
-            int desiredWidth = View.MeasureSpec.makeMeasureSpec(mListView.getWidth(), View.MeasureSpec.UNSPECIFIED);
-            for (int i = 0; i < mListAdapter.getCount(); i++) {
-                View listItem = mListAdapter.getView(i, null, mListView);
+
+            int totalHeight = 0;
+            int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+            for (int i = 0; i < listAdapter.getCount(); i++) {
+                View listItem = listAdapter.getView(i, null, listView);
                 listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-                height += listItem.getMeasuredHeight();
+                totalHeight += listItem.getMeasuredHeight();
             }
-            ViewGroup.LayoutParams params = mListView.getLayoutParams();
-            params.height = height + (mListView.getDividerHeight() * (mListAdapter.getCount() - 1));
-            mListView.setLayoutParams(params);
-            mListView.requestLayout();
+
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+            listView.setLayoutParams(params);
+            listView.requestLayout();
         }
     }
 
